c305da856b6afd8aa26afba223701353c0cbc655
SondreB <sondre@outlook.com>
2022-12-22 21:59:10 +0100

    Add the nostrich

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -4,6 +4,7 @@ import { ApplicationState } from '../services/applicationstate.service';
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
+  styleUrls: ['./help.component.css'],
 })
 export class HelpComponent {
   constructor(private appState: ApplicationState) {
