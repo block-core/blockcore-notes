f9566eaa6c1c6fa7e1b4fcc235aacbea32e4f75c
Milad <miladsoft@yahoo.com>
2022-12-23 18:26:50 +0330

    Add dialog to create new Note

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { AuthenticationService } from './services/authentication.service';
 import { AppUpdateService } from './services/app-update.service';
 import { CheckForUpdateService } from './services/check-for-update.service';
 import { StorageService } from './services/storage.service';
+import { MatDialog } from '@angular/material/dialog';
+import { NoteDialog } from './shared/create-note-dialog/create-note-dialog';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +18,9 @@ export class AppComponent {
   @ViewChild('drawer') drawer!: MatSidenav;
   @ViewChild('draweraccount') draweraccount!: MatSidenav;
   authenticated = false;
+  note: any;
 
-  constructor(public appState: ApplicationState, private storage: StorageService, public authService: AuthenticationService, private router: Router, public appUpdateService: AppUpdateService, public appUpdateCheckService: CheckForUpdateService) {
+  constructor(public appState: ApplicationState, private storage: StorageService, public authService: AuthenticationService, private router: Router, public appUpdateService: AppUpdateService, public appUpdateCheckService: CheckForUpdateService, public dialog: MatDialog) {
     appState.title = 'Blockcore Notes';
 
     this.authService.authInfo$.subscribe((auth) => {
@@ -35,5 +38,15 @@ export class AppComponent {
 
     // const testdata = await this.storage.get('123');
     // console.log(testdata);
+  }
+
+  createNote(): void {
+    const dialogRef = this.dialog.open(NoteDialog, {
+      data: {name: this.note},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.note = result;
+    });
   }
 }
