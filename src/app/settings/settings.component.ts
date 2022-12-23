e65665803435961c38d927783c5fabe5c4924f35
SondreB <sondre@outlook.com>
2022-12-23 16:12:35 +0100

    Add settings action to wipe entire local database cache

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { ApplicationState } from '../services/applicationstate.service';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
 })
 export class SettingsComponent {
-  constructor(private appState: ApplicationState) {
+
+  wiped = false;
+
+  constructor(private appState: ApplicationState, private storage: StorageService) {
     appState.showBackButton = true;
     appState.title = 'Settings';
+  }
+
+  async clearDatabase() {
+    await this.storage.wipe();
+    this.wiped = true;
   }
 }
