903baefff45496b295fa908eedd957734ea40df7
SondreB <sondre@outlook.com>
2022-12-25 02:41:18 +0100

    Add a basic for implementing schema validation using Joi

---
+++ src/app/schemas/nostr-event.ts
@@ -0,0 +1,5 @@
+// import Schema from 'joi';
+
+// TODO: Do something similar to nostream implementation of schema validation: https://github.com/Cameri/nostream/tree/main/src/schemas
+
+// TODO: Also make another consideration if we should use ajv instead of joi?
