bc963300d7b6c8b8e5e08de7178a25fb21f97300
SondreB <sondre@outlook.com>
2022-12-31 01:15:49 +0100

    Display npub in tooltip on profile name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -18,11 +18,12 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent {
   pubkey?: string | null;
-  npub?: string;
+  npub!: string;
   profile?: NostrProfileDocument;
   about?: string;
   imagePath = '/assets/profile.png';
