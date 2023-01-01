9f434d637bec51ddcdc87803b6344851953d59ab
SondreB <sondre@outlook.com>
2023-01-01 01:14:05 +0100

    Add a maximize toggle to dialog control

--- src/app/shared/create-note-dialog/create-note-dialog.ts
+++ src/app/shared/create-note-dialog/create-note-dialog.ts
@@ -11,6 +11,8 @@ export interface NoteDialogData {
   styleUrls: ['create-note-dialog.scss'],
 })
 export class NoteDialog {
+  isEmojiPickerVisible: boolean | undefined;
+
   constructor(public dialogRef: MatDialogRef<NoteDialog>, @Inject(MAT_DIALOG_DATA) public data: NoteDialogData) {
     this.data.note = '';
   }
@@ -20,9 +22,27 @@ export class NoteDialog {
     this.dialogRef.close();
   }
 
-  public isEmojiPickerVisible: boolean | undefined;
   public addEmoji(event: { emoji: { native: any } }) {
     this.data.note = `${this.data.note}${event.emoji.native}`;
     this.isEmojiPickerVisible = false;
+  }
+
+  largeSize = false;
+
+  toggleSize() {
+    if (this.largeSize) {
+      this.dialogRef.updateSize('auto', 'auto');
+    } else {
+      this.dialogRef.updateSize('100vw', '100vh');
+    }
+
+    this.largeSize = !this.largeSize;
+
+    // .full-width-dialog {
+    //   max-width: 100vw !important;
+    //   height: 100%;
+    //   width: 100%;
+    //   min-width: 0;
+    // }
   }
 }
