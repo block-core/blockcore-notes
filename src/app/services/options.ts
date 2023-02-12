6cdbe878efc3b19c462b4d6a50be069e1ed6b80f
SondreB <sondre@outlook.com>
2023-02-12 02:01:31 +0100

    Improve default values for people view

--- src/app/services/options.ts
+++ src/app/services/options.ts
@@ -16,8 +16,6 @@ export interface Options {
   primaryRelay?: string;
   peopleDisplayView: number;
   peopleDisplayType: number;
-  // peopleDisplayAbout: boolean;
-  // peopleDisplayDate: boolean;
   peopleDisplaySort?: string;
 }
 
@@ -29,12 +27,24 @@ export class OptionsService {
     this.load();
   }
 
-  values: Options = { showLines: true, peopleDisplayType: 1, peopleDisplayView: 0 };
+  values: Options = { showLines: true, peopleDisplayType: 1, peopleDisplayView: 0, peopleDisplaySort: 'name-asc' };
 
   load() {
     let options = localStorage.getItem('blockcore:notes:nostr:options');
     if (options) {
       this.values = JSON.parse(options);
+    }
+
+    if (this.values.peopleDisplayType == null) {
+      this.values.peopleDisplayType = 1;
+    }
+
+    if (this.values.peopleDisplayView == null) {
+      this.values.peopleDisplayView = 0;
+    }
+
+    if (this.values.peopleDisplaySort == null) {
+      this.values.peopleDisplaySort = 'name-asc';
     }
   }
 
