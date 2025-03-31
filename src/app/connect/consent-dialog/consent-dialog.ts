d007899e60556db47f848c0c814e8bdff4e4c92a
SondreB <sondre@outlook.com>
2025-03-31 19:32:51 +0200

    Add Angular Material modules and shared components to chat and consent-dialog components

--- src/app/connect/consent-dialog/consent-dialog.ts
+++ src/app/connect/consent-dialog/consent-dialog.ts
@@ -1,10 +1,11 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'add-consent-dialog',
   templateUrl: 'consent-dialog.html',
   styleUrls: ['consent-dialog.css'],
+  imports: [MatDialogModule],
 })
 export class ConsentDialog {
   constructor(public dialogRef: MatDialogRef<boolean>, @Inject(MAT_DIALOG_DATA) public consent: boolean) {}
