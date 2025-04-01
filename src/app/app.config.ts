bfdbf87be8b02fab63f784fe12bc4f1eb45110f9
SondreB <sondre@outlook.com>
2025-04-01 12:52:46 +0200

    Implement custom TranslateLoader for ngx-translate; update app.config.ts and remove HttpClient dependency; add Google Fonts and Material Icons to index.html; adjust styles.scss to comment out Material Icons import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,13 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Observable, from } from 'rxjs';
+import { CheckForUpdateService } from './services/check-for-update';
+import { AppUpdateService } from './services/app-update';
+import { AuthGuardService } from './services/auth-guard';
+import { LoggerModule, NgxLoggerLevel, TOKEN_LOGGER_WRITER_SERVICE } from '@blockcore/ngx-logger';
+import { LogWriterService } from './services/log-writer';
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
   new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -16,7 +22,25 @@ const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: Http
 // export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 //   return new TranslateHttpLoader(http);
 // }
+// Custom TranslateLoader implementation using fetch
+class FetchTranslateLoader implements TranslateLoader {
+  constructor(private prefix: string = './assets/i18n/', private suffix: string = '.json') {}
 
+  getTranslation(lang: string): Observable<any> {
+    const promise = fetch(`${this.prefix}${lang}${this.suffix}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error ${response.status}`);
+        }
+        return response.json();
+      });
+    
+    return from(promise);
+  }
+}
+
+// Factory function that returns our custom loader
+const fetchLoaderFactory = () => new FetchTranslateLoader('./assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,10 +49,27 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom([TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient],
+        useFactory: fetchLoaderFactory,
       },
+      // Removed deps: [HttpClient] since we don't need it anymore
     })]),
+    // importProvidersFrom([TranslateModule.forRoot({
+    //   loader: {
+    //     provide: TranslateLoader,
+    //     useFactory: httpLoaderFactory,
+    //     deps: [HttpClient],
+    //   },
+    // })]),
+    
+    importProvidersFrom(LoggerModule.forRoot({ level: NgxLoggerLevel.INFO, enableSourceMaps: true, serverLogLevel: NgxLoggerLevel.OFF }, // Don't send logs anywhere!
+      {
+          writerProvider: {
+              provide: TOKEN_LOGGER_WRITER_SERVICE,
+              useClass: LogWriterService,
+          },
+      }))
+    ,
+    AppUpdateService, CheckForUpdateService, AuthGuardService, 
     importProvidersFrom(BrowserAnimationsModule),
     { provide: MAT_CARD_CONFIG, useValue: { appearance: 'outlined' } },
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
