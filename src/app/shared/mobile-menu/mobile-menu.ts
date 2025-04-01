430f74a2edf856bb6f0d7de518e726265b3ff49f
SondreB <sondre@outlook.com>
2025-04-02 01:07:45 +0200

    Enhance mobile menu with badge support for notifications and refactor button structure for improved styling

--- src/app/shared/mobile-menu/mobile-menu.ts
+++ src/app/shared/mobile-menu/mobile-menu.ts
@@ -8,17 +8,20 @@ import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { TranslateModule } from '@ngx-translate/core';
 import { ApplicationState } from 'src/app/services/applicationstate';
+import { MatBadgeModule } from '@angular/material/badge';
+import { UIService } from 'src/app/services/ui';
 
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.html',
   styleUrls: ['./mobile-menu.css'],
-  imports: [MatToolbarModule, TranslateModule, RouterModule, MatButtonModule, MatIconModule, CommonModule, MatSliderModule, FormsModule],
+  imports: [MatToolbarModule, MatBadgeModule, TranslateModule, RouterModule, MatButtonModule, MatIconModule, CommonModule, MatSliderModule, FormsModule],
 })
 export class MobileMenuComponent {
   @Input() miniplayer = false;
 
   appState = inject(ApplicationState);
+  ui = inject(UIService);
 
   constructor() {}
 
