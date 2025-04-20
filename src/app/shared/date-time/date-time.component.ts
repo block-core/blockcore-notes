120df60cc1a55bc5f44c291d982358b966cac79f
wixhub <rublin@gmx.de>
2025-04-20 17:11:10 +0200

    data binding

--- src/app/shared/date-time/date-time.component.ts
+++ src/app/shared/date-time/date-time.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { combineLatest, map } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { MatTimepickerModule } from '@angular/material/timepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,6 +10,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-date-time',
+  standalone: true,
   templateUrl: './date-time.component.html',
   styleUrls: ['./date-time.component.scss'],
   providers: [provideNativeDateAdapter()],
@@ -19,27 +21,66 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     MatDatepickerModule,
     ReactiveFormsModule,
     FormsModule,
+    TranslateModule
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DateTimeComponent {
+export class DateTimeComponent implements OnInit, OnDestroy {
 
-  dateControl = new FormControl();
-  timeControl = new FormControl();
+  @Input() initialDateTime?: Date;
+  @Input() dateFormat?: string = 'MM/DD/YYYY';
+  @Input() minDate?: Date;
+  @Input() maxDate?: Date;
 
-  combinedValue = combineLatest([
-    this.dateControl.valueChanges,
-    this.timeControl.valueChanges,
-  ]).pipe(
-    map(([date, time]) => {
-      if (date && time) {
-        const combined = new Date(date);
-        combined.setHours(time.getHours());
-        combined.setMinutes(time.getMinutes());
-        combined.setSeconds(time.getSeconds());
-        return combined;
-      }
-      return null;
-    })
-  );
+  @Input() CustomLabel?: string = '';
+  @Input() CustomDateLabel?: string = 'Date';
+  @Input() CustomTimeLabel?: string = 'Time';
+  @Input() CustomDateHint?: string = '';
+  @Input() CustomHoursHint?: string = '';
+
+  @Output() dateTimeChanged = new EventEmitter<Date | null>();
+
+  dateTimeForm: FormGroup;
+
+  constructor() {
+    this.dateTimeForm = new FormGroup({
+      dateControl: new FormControl(null),
+      timeControl: new FormControl(null),
+    });
+  }
+
+  private subscriptions = new Subscription();
+
+  ngOnInit(): void {
+    if (this.initialDateTime) {
+      this.dateTimeForm.controls['dateControl'].setValue(this.initialDateTime);
+      this.dateTimeForm.controls['timeControl'].setValue(this.initialDateTime);
+    }
+
+    this.subscriptions.add(
+      this.dateTimeForm.valueChanges.subscribe((values) => {
+        if (values.dateControl || values.timeControl) { // Check if at least one has a value
+          this.emitCombinedDateTime(values.dateControl, values.timeControl);
+        } else {
+          this.dateTimeChanged.emit(null); // Optionally emit null if both are cleared
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private emitCombinedDateTime(date: Date | null, time: Date | null): void {
+    let combinedDateTime: Date | null = null;
+    if (date && time) {
+      combinedDateTime = new Date(date);
+      combinedDateTime.setHours(time.getHours());
+      combinedDateTime.setMinutes(time.getMinutes());
+    } else if (date) {
+      combinedDateTime = date;
+    }
+    this.dateTimeChanged.emit(combinedDateTime);
+  }
 }
